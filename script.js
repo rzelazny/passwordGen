c939b04ded9281ec4adb917e4d10f229a213e86d
Ryan Zelazny <RyanZelazny@gmail.com>
2020-10-26 15:02:52 -0400

    collecting parameters from user

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Generates a passcode to user specifications
+function generatePassword(){
+
+  // Prompt user for password length and validate size to 8-128
+  do{
+    var passLength = prompt("How long should the password be? (8-128)");
+
+    if(passLength < 8){
+      alert("Minimum password length is 8");
+    }
+    if(passLength > 128){
+      alert("Maximum password length is 128");
+    }
+  }while (passLength < 8 || passLength > 128);
+  
+
+
+
+  //Prompt user for password parameters
+  var useLower = confirm("Should the password have lower case characters?");
+  var useUpper = confirm("Should the password have upper case characters?");
+  var useNumeric = confirm("Should the password have numeric characters?");
+  var useSpecial = confirm("Should the password have special characters?");
+
+}
