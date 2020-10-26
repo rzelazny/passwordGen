5395b8bc57e311c022b3364be2924b5c4ee6fc2c
Ryan Zelazny <RyanZelazny@gmail.com>
2020-10-26 18:45:07 -0400

    escaped last special characters

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ function generatePassword(){
     useableCharacters += "0123456789";
   }
   if (useSpecial){
+    //Escape " and \ 
     useableCharacters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-    console.log(useableCharacters);
   }
   //Set length of useable character string
   var useableLength = useableCharacters.length;
